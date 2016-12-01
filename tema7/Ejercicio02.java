9ae2c463a37482b81b1dc08814017d8af285162c
JoaquinBelloJimenez <joaquin.bj.97@gmail.com>
2016-12-01 12:51:55 +0100

    Array otro más

---
+++ tema7/Ejercicio02.java
@@ -0,0 +1,24 @@
+/*
+ *Joaquín Bello Jiménez 
+ */
+
+
+public class Ejercicio02 {
+	
+	public static void main (String args[]) {
+    
+    String[] simbolo = new String[10];
+          simbolo[0] = "a";
+          simbolo[1] = "x";
+          simbolo[4] = "@";
+          simbolo[6] = "''";
+          simbolo[7] = "+";
+          simbolo[8] = "Q";
+          
+    for(int i=0;i < 10;i++){
+      System.out.println(i + ": " + simbolo[i]);
+    }
+    System.out.print("Se inicializan como \"null\"");
+	}
+}
+
