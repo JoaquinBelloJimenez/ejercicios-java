d014923302ed16dfcb98463bf2d2d03957422450
JoaquinBelloJimenez <joaquin.bj.97@gmail.com>
2016-12-01 12:33:46 +0100

    Array más ejercicios

---
+++ tema7/Ejercicio01.java
@@ -0,0 +1,24 @@
+/*
+ *Joaquín Bello Jiménez 
+ */
+
+
+public class Ejercicio01 {
+	
+	public static void main (String args[]) {
+    
+    int[] num = new int[12];
+          num[0] = 39;
+          num[1] = -2;
+          num[4] = 0;
+          num[6] = 14;
+          num[8] = 5;
+          num[9] = 120;
+          
+    for(int i=0;i < 12;i++){
+      System.out.println(i + ": " + num[i]);
+    }
+    System.out.print("Se inicializan como 0");
+	}
+}
+
