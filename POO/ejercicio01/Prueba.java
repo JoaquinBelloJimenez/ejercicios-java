07a2377d4a52c46172576dc52cb081204431143f
JoaquinBelloJimenez <joaquin.bj.97@gmail.com>
2017-01-31 10:01:33 +0100

    ejercicio poo 1

---
+++ POO/ejercicio01/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ *Joaquín Bello Jiménez 
+ */
+
+public class Prueba {
+  
+  public static void main (String[] args){
+  
+  Caballo a = new Caballo("juanito", "naranja", 112, 8);
+  Caballo b = new Caballo("pichon", "marrón", 132, 12);
+
+  
+  a.cumple();
+  a.reir();
+  
+  
+  
+  
+  }
+}
